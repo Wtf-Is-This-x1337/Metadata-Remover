328631f66f09b4228c0252aaca5b80a0497bc934
Andrei <78364462+Wtf-Is-This-x1337@users.noreply.github.com>
2021-04-03 03:38:17 -1000

    Update GPS_To_Coordinates.py

--- GPS_To_Coordinates.py
+++ GPS_To_Coordinates.py
@@ -12,12 +12,7 @@ if args.lat and args.lon:
 	dlat = int(lat[0]) + (int(lat[1])/60.0) + (int(lat[2])/3600.0)
 	dlon = int(lon[0]) + (int(lon[1])/60.0) + (int(lon[2])/3600.0)
 
-	print dlat, dlon
+	print(dlat, dlon)
 
 else:
-	print parser.usage
-
-
-
-
-
+	print(parser.usage)
